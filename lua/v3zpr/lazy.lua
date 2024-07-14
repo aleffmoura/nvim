d8f5f5733c20e5e555ea2ad240bbd6b2a19079d2
v3zpr <aleffmds@gmail.com>
2024-07-14 17:32:31 -0300

    adicionado todos os plugins basicos

--- lua/v3zpr/lazy.lua
+++ lua/v3zpr/lazy.lua
@@ -1,16 +1,22 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
-
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("v3zpr/plugins")
-
+require("lazy").setup({ { import = "v3zpr.plugins" }, { import = "v3zpr.plugins.lsp" } }, {
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
+})
